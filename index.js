3a3a91817e93c725165dec6c7065db073ee31574
mintcarrotkeys <96617003+mintcarrotkeys@users.noreply.github.com>
2022-01-05 20:22:05 +1100

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,85 @@
 
 
-document.getElementById("test").onclick = function requestCode() {
 
-    console.log("Hi")
+
+async function requestToken() {
+    const codeVerifier = localStorage.getItem('handle_login_verifier');
+    const state = localStorage.getItem('handle_login_state');
+    const params = new URLSearchParams(url.toString().split("?")[1]);
+
+    if (params.has('code') === false) {
+        return "";
+    }
+    const code = params.get('code');
+    console.log(code);
+    const requestURL = (
+        "https://student.sbhs.net.au/api/token?" +
+        ""
+
+    );
+
+}
+
+requestToken();
+
+
+
+
+document.getElementById("test").onclick = async function requestCode() {
+    //generate code verifier 43-128 characters long
+    function randomString(length) {
+        let randomNumbers = new Uint32Array(length);
+        let verifier = "";
+        const allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~";
+        crypto.getRandomValues(randomNumbers);
+        for (const i of randomNumbers) {
+            verifier += allowedChars.charAt((i % 66));
+        }
+        return verifier;
+    }
+    async function digestMessage(message) {
+        const encoder = new TextEncoder();
+        const data = encoder.encode(message);
+        const hash = await crypto.subtle.digest('SHA-256', data);
+        return hash;
+    }
+    function base64url(input) {
+        let hashString = "";
+        for (const i of input) {
+            hashString += String.fromCharCode(i);
+        }
+        return btoa(hashString).replaceAll("=", "").replaceAll("+", "-").replaceAll("/", "_");
+    }
+    const codeVerifier = randomString(128);
+    const hash = await digestMessage(codeVerifier);
+    const viewHash = new Uint8Array(hash);
+    const codeChallenge = base64url(viewHash);
+    const state = randomString(32);
+    localStorage.setItem('handle_login_verifier', codeVerifier);
+    localStorage.setItem('handle_login_state', state);
+
+    const redirect = encodeURIComponent('https://testing-genericbells.pages.dev');
+
+    // generate code challenge from code verifier
+    //
+    //
+    //        "&redirect_uri=https://testing-genericbells.pages.dev"
+    // https://student.sbhs.net.au/api/authorize&client_id=genericbellstestingonly
+    // &response_type=code&state=DLK1b44s4BWk~ys5holF7Vm7m8Eiy_Re
+    // &code_challenge=u2lOoOGYBWoOW46exdRPTX7g0qNUHVE7VS6iUJowCBw
+    // &code_challenge_method=S256&scope=all-ro
+
+    const requestURL = (
+        "https://student.sbhs.net.au/api/authorize?" +
+        "client_id=genericbellstestingonly&" +
+        "response_type=code&" +
+        "state=" + state + "&" +
+        "code_challenge=" + codeChallenge + "&" +
+        "code_challenge_method=S256&" +
+        "scope=all-ro&" +
+        "redirect_uri=" + redirect
+    );
+
+    location.assign(requestURL);
 
 };
